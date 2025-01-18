759d97b10988ed4cd8c396f04c1c411236037ba6
R4f1lds <rafilds66@gmail.com>
2025-01-18 16:59:26 -0300

    terceiro commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,17 @@ const inputs = document.querySelectorAll(".input")
 const botao = document.querySelector(".botao")
 const invalido = document.querySelectorAll(".invalido")
 
-const validacao = () => {
+const validar = () => {
    inputs.forEach((inputs, index) => { 
     if(inputs.value === ""){
        inputs.classList.remove("valido")
        inputs.classList.add("erro")
-       invalido[index].innerHTML = `Campo obrigatorio`
+       invalido[index].innerHTML = "Campo obrigatorio"
     } else {
         inputs.classList.remove("erro")
         inputs.classList.add("valido")
-        invalido[index].innerHTML = ``
+        invalido[index].innerHTML = ""
     }
 })}
 
-botao.addEventListener('click', validacao)
+botao.addEventListener("click", validar)
